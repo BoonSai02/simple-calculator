75cdc552acb5bd63a5102a46adf33e891ca69914
Boon-Sai <boonsaikalathur@gmail.com>
2026-04-09 16:54:31 +0530

    Added frontend container + multi-container setup

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,51 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#  Allow frontend (important for browser requests)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CalcInput(BaseModel):
     a: float
     b: float
 
+
 @app.get("/")
 def home():
-    return {
-        "message": "Calculator API is running..."
-    }
+    return {"message": "Calculator API is running..."}
+
 
 @app.post("/add")
 def add(data: CalcInput):
-    return {
-        "result": data.a + data.b
-    }
+    return {"result": data.a + data.b}
+
 
 @app.post("/sub")
 def sub(data: CalcInput):
-    return {
-        "result": data.a - data.b
-    }
-@app.post("/mult")
-def mult(data: CalcInput):
-    return {
-        "result": data.a * data.b
-    }
+    return {"result": data.a - data.b}
+
+
+@app.post("/mul")
+def mul(data: CalcInput):
+    return {"result": data.a * data.b}
+
 
 @app.post("/div")
 def div(data: CalcInput):
-    return {
-        "result": data.a / data.b
-    }
+    # optional safe check
+    if data.b == 0:
+        return {"error": "Division by zero not allowed"}
+    return {"result": data.a / data.b}
+
 
 @app.post("/pow")
-def pow(data: CalcInput):
-    return {
-        "result": data.a ** data.b
-    }
-
-
+def power(data: CalcInput):
+    return {"result": data.a ** data.b}
